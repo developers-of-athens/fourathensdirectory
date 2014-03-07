170720e47d12264032ea4b293e115cdcd5037f35
Jordan Burke <me@jordankburke.com>
2014-03-07 17:54:56 -0500

    Added office model and assorted gems. Closes #5.

---
+++ db/migrate/20140307225318_create_contacts.rb
@@ -0,0 +1,20 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :company_name
+      t.integer :parent_company_id
+      t.string :phone_number
+      t.boolea :allows_text
+      t.string :n
+      t.string :email
+      t.string :url
+      t.text :description
+      t.boolean :is_main
+      t.references :office, index: true
+
+      t.timestamps
+    end
+  end
+end
