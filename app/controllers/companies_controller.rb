a6342d5686503b9092962b28dce3f72065ddd63e
Jordan Burke <me@jordankburke.com>
2014-03-07 20:40:03 -0500

    Updated controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,6 @@
 class CompaniesController < ApplicationController
   respond_to :json
   before_action :set_company, only: [:show, :edit, :update, :destroy]
-  before_filter :check_format
 
   # GET /companies
   # GET /companies.json
@@ -24,9 +23,5 @@ class CompaniesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
       params[:company]
-    end
-
-    def check_format
-      render :nothing => true, :status => 406 unless params[:format] == 'json'
     end
 end
--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,7 +1,6 @@
 class OfficesController < ApplicationController
   respond_to :json
   before_action :set_office, only: [:show, :edit, :update, :destroy]
-  before_filter :check_format
 
   # GET /offices.json
   def index
@@ -22,9 +21,5 @@ class OfficesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def office_params
       params[:office]
-    end
-
-    def check_format
-      render :nothing => true, :status => 406 unless params[:format] == 'json'
     end
 end
