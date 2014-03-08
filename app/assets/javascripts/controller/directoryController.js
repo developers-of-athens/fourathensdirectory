6545f5f805fbb5580abe131017c583d81c6626da
Glenn Stovall <glennstovall@gmail.com>
2014-03-07 20:53:19 -0500

    added directory data pulls

--- app/assets/javascripts/controller/directoryController.js
+++ app/assets/javascripts/controller/directoryController.js
@@ -1,7 +1,10 @@
 dir.controller('directoryController', ['$scope', '$http', function($scope, $http) {
+	$scope.contacts = [];
 	$http({method : 'GET', url: '/contacts.json'})
 		.success(function(data, status, headers, config) {
 			//data
 			console.log(data);
+			$scope.contacts = data;
 		});
+
 }]);
